59f5ca4768e03d1a6b51d77d59c1972772376661
shameem-flutter <shameem.m.a999@gmail.com>
2024-11-13 15:33:49 +0530

    Add files via upload

---
+++ reversed.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  print('enter a sentence');
+  String sentence = stdin.readLineSync()!;
+  reverseWords(sentence);
+}
+
+void reverseWords(sentence) {
+  List words = sentence.split(' ');
+  words = words.reversed.toList();
+  String reversedsentence = words.join(' ');
+  print(reversedsentence);
+}
